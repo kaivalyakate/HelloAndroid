d084010df3a31a95db12672bef7217e471aeb232
Kaiwalya Kate <6522kaivalya@gmail.com>
2022-01-25 22:22:14 -0500

    Initial commit

---
+++ app/src/main/java/edu/stevens/cs522/hello/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.stevens.cs522.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static String mainActivityId = "edu.stevens.cs522.hello";
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText edText = findViewById(R.id.editTextTextPersonName);
+        String helloText = "Hello "+edText.getText();
+        Intent intent = new Intent(this, ShowActivity.class);
+        intent.putExtra(mainActivityId, helloText);
+        startActivity(intent);
+    }
+
+//    public void onBtnClick(View view){
+//        EditText ed = findViewById(R.id.editTextTextPersonName);
+//        System.out.println(ed.getText());
+//        String output = "Hello "+ed.getText();
+//        Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
+//    }
+
+}
---
+++ app/src/main/java/edu/stevens/cs522/hello/ShowActivity.java
@@ -0,0 +1,26 @@
+package edu.stevens.cs522.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.mainActivityId);
+
+        TextView tv = findViewById(R.id.welcomeTextView);
+        tv.setText(message);
+    }
+}
